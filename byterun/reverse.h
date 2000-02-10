0a7d9dd867e770012d1f85e9f5cf837c28c8e919
Xavier Leroy <xavier.leroy@inria.fr>
2000-02-10 14:03:09 +0000

    Generaliser les operations Reverse
    (cherry picked and forward-ported from upstream commit 7175ab048dcaaa39649ebc386ae37750baaf27e1)

--- byterun/reverse.h
+++ byterun/reverse.h
@@ -12,12 +12,18 @@
 /***********************************************************************/
 /*e: copyright header C xavier */
 
-/* Swap byte-order in 32-bit integers and in words */
+/* claude: upstream (7175ab04, 2000-02-10) generalizes these to 2-arg
+ * dst/src macros and adds a 16-bit variant; old 1-arg bodies kept below
+ * under #if 0 rather than deleted, since the syncweb markers wrapping
+ * them must not be renamed/moved (see docs/literate) */
+
+/* Swap byte-order in 16, 32, and 64-bit integers or floats */
 
 #ifndef _reverse_
 #define _reverse_
 
 /*s: function [[Reverse_int32]] */
+#if 0
 #define Reverse_int32(w) {                                                    \
   char * _p;                                                                  \
   int _a;                                                                     \
@@ -29,9 +35,23 @@
   _p[1] = _p[2];                                                              \
   _p[2] = _a;                                                                 \
 }
+#endif
+#define Reverse_32(dst,src) {                                               \
+  char * _p, * _q;                                                          \
+  char _a, _b;                                                              \
+  _p = (char *) (src);                                                      \
+  _q = (char *) (dst);                                                      \
+  _a = _p[0];                                                               \
+  _b = _p[1];                                                               \
+  _q[0] = _p[3];                                                            \
+  _q[1] = _p[2];                                                            \
+  _q[3] = _a;                                                               \
+  _q[2] = _b;                                                               \
+}
 /*e: function [[Reverse_int32]] */
 
 /*s: function [[Reverse_int64]] */
+#if 0
 #define Reverse_int64(d) {                                                    \
   char * _p;                                                                  \
   int _a;                                                                     \
@@ -49,10 +69,40 @@
   _p[3] = _p[4];                                                              \
   _p[4] = _a;                                                                 \
 }
+#endif
+#define Reverse_64(dst,src) {                                               \
+  char * _p, * _q;                                                          \
+  char _a, _b;                                                              \
+  _p = (char *) (src);                                                      \
+  _q = (char *) (dst);                                                      \
+  _a = _p[0];                                                               \
+  _b = _p[1];                                                               \
+  _q[0] = _p[7];                                                            \
+  _q[1] = _p[6];                                                            \
+  _q[7] = _a;                                                               \
+  _q[6] = _b;                                                               \
+  _a = _p[2];                                                               \
+  _b = _p[3];                                                               \
+  _q[2] = _p[5];                                                            \
+  _q[3] = _p[4];                                                            \
+  _q[5] = _a;                                                               \
+  _q[4] = _b;                                                               \
+}
 /*e: function [[Reverse_int64]] */
 
 /*s: constant [[Reverse_double]] */
+#if 0
 #define Reverse_double Reverse_int64
+#endif
+#define Reverse_16(dst,src) {                                               \
+  char * _p, * _q;                                                          \
+  char _a;                                                                  \
+  _p = (char *) (src);                                                      \
+  _q = (char *) (dst);                                                      \
+  _a = _p[0];                                                               \
+  _q[0] = _p[1];                                                            \
+  _q[1] = _a;                                                               \
+}
 /*e: constant [[Reverse_double]] */
 
 
