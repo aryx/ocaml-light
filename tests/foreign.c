3ca0911bef6330c3299c0c9d3351f39b96cd4b31
pad <pad@fb.com>
2015-05-08 16:58:40 -0700

    * tests/test_foreign.ml: expose backtrace bug when exn thrown from C

--- tests/foreign.c
+++ tests/foreign.c
@@ -4,10 +4,16 @@
 #include <caml/mlvalues.h>
 #include <caml/memory.h>
 
-
 void caml_test_foreign(value x, value y) {
   CAMLparam2(x, y);
 
   CAMLreturn0;
+}
+
+
+extern void invalid_argument (char *);
+
+void caml_test_foreign2() {
+  invalid_argument("Array.get");
 }
 
--- tests/test_foreign.ml
+++ tests/test_foreign.ml
@@ -1,5 +1,15 @@
 
 external test_foreign: int -> int -> unit = "caml_test_foreign"
+external test_foreign2: unit -> unit = "caml_test_foreign2"
+
+(* export CAMLRUNPARAM=b and then run this program.
+ * This used to cause some segfaults
+ *)
+
+let foo () =
+  let _ = test_foreign2 () in
+  ()
+
 
 let test () =
   Printf.printf "Before\n";
@@ -7,7 +17,13 @@ let test () =
   let _ = Array.make 10000 (Some 2) in
   Gc.minor();
   Printf.printf "After\n";
-  ()
+  (try 
+    foo ();
+  with e ->
+    let s = Printexc.get_backtrace () in
+    print_string s;
+    Printf.printf "Really After\n";
+  )
 
 
 let _ =
