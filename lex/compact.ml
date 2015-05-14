91428882989e179d292d85a625071b0d94ce6674
pad <pad@fb.com>
2015-05-13 17:02:00 -0700

    * lex/compact.ml: lpized a bit

--- lex/compact.ml
+++ lex/compact.ml
@@ -28,9 +28,13 @@ let most_frequent_elt v =
       try
         Hashtbl.find frequencies e
       with Not_found ->
-        let r = ref 1 in Hashtbl.add frequencies e r; r in
+        let r = ref 1 in Hashtbl.add frequencies e r; r 
+    in
     incr r;
-    if !r > !max_freq then begin max_freq := !r; most_freq := e end
+    if !r > !max_freq then begin 
+      max_freq := !r; 
+      most_freq := e 
+    end
   done;
   !most_freq
 (*e: function Compact.most_frequent_elt *)
@@ -40,7 +44,9 @@ let most_frequent_elt v =
 
 let non_default_elements def v =
   let rec nondef i =
-    if i >= Array.length v then [] else begin
+    if i >= Array.length v 
+    then [] 
+    else begin
       let e = v.(i) in
       if e = def then nondef(i+1) else (i, e) :: nondef(i+1)
     end in
@@ -49,9 +55,16 @@ let non_default_elements def v =
 
 (* Compact the transition and check arrays *)
 
+(*s: global Compact.trans *)
 let trans = ref(Array.create 1024 0)
-and check = ref(Array.create 1024 (-1))
-and last_used = ref 0
+(*e: global Compact.trans *)
+(*s: global Compact.check *)
+let check = ref(Array.create 1024 (-1))
+(*e: global Compact.check *)
+(*s: global Compact.last_used *)
+let last_used = ref 0
+(*e: global Compact.last_used *)
+
 
 (*s: function Compact.grow_transitions *)
 let grow_transitions () =
@@ -73,22 +86,28 @@ let pack_moves state_num move_t =
         Backtrack -> -1
       | Goto n -> n)
   done;
+
   let default = most_frequent_elt move_v in
   let nondef = non_default_elements default move_v in
+
   let rec pack_from b =
-    while b + 257 > Array.length !trans do grow_transitions() done;
+    while b + 257 > Array.length !trans do 
+      grow_transitions() 
+    done;
     let rec try_pack = function
       [] -> b
     | (pos, v) :: rem ->
         if !check.(b + pos) = -1 then try_pack rem else pack_from (b+1) in
-    try_pack nondef in
+    try_pack nondef 
+  in
   let base = pack_from 0 in
-  List.iter
-    (fun (pos, v) ->
+
+  nondef |> List.iter (fun (pos, v) ->
       !trans.(base + pos) <- v;
-      !check.(base + pos) <- state_num)
-    nondef;
-  if base + 257 > !last_used then last_used := base + 257;
+      !check.(base + pos) <- state_num
+  );
+  if base + 257 > !last_used 
+  then last_used := base + 257;
   (base, default)
 (*e: function Compact.pack_moves *)
 
@@ -96,20 +115,27 @@ let pack_moves state_num move_t =
 (* Compaction of an automata *)
 
 type lex_tables =
-  { tbl_base: int array;                 (* Perform / Shift *)
+  { 
+    (* negative int -(n+1)  for Perform n, idx in tbl_trans for Shift *)
+    tbl_base: int array;                 (* Perform / Shift *)
+    (* -1 = No_remember, positive = Remember n *)
     tbl_backtrk: int array;              (* No_remember / Remember *)
-    tbl_default: int array;              (* Default transition *)
 
+    tbl_default: int array;              (* Default transition *)
     tbl_trans: int array;                (* Transitions (compacted) *)
-    tbl_check: int array }               (* Check (compacted) *)
+    tbl_check: int array;                (* Check (compacted) *)
+  }
 (*e: type Compact.lex_tables *)
 
 (*s: function Compact.compact_tables *)
 let compact_tables state_v =
   let n = Array.length state_v in
+
   let base = Array.create n 0 in
   let backtrk = Array.create n (-1) in
   let default = Array.create n 0 in
+
+
   for i = 0 to n - 1 do
     match state_v.(i) with
       Perform n ->
@@ -127,7 +153,8 @@ let compact_tables state_v =
     tbl_backtrk = backtrk;
     tbl_default = default;
     tbl_trans = Array.sub !trans 0 !last_used;
-    tbl_check = Array.sub !check 0 !last_used }
+    tbl_check = Array.sub !check 0 !last_used;
+  }
 (*e: function Compact.compact_tables *)
 
 (*e: lex/compact.ml *)
--- lex/compact.mli
+++ lex/compact.mli
@@ -15,12 +15,16 @@
 (* Compaction of an automata *)
 
 type lex_tables =
-  { tbl_base: int array;                 (* Perform / Shift *)
+  { 
+    (* negative int -(n+1)  for Perform n, idx in tbl_trans for Shift *)
+    tbl_base: int array;                 (* Perform / Shift *)
+    (* -1 = No_remember, positive = Remember n *)
     tbl_backtrk: int array;              (* No_remember / Remember *)
-    tbl_default: int array;              (* Default transition *)
 
+    tbl_default: int array;              (* Default transition *)
     tbl_trans: int array;                (* Transitions (compacted) *)
-    tbl_check: int array }               (* Check (compacted) *)
+    tbl_check: int array;                (* Check (compacted) *)
+  }
 (*e: type Compact.lex_tables *)
 
 (*s: signature Compact.compact_tables *)
