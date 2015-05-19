a38ebb05b4e554c8419c2ad37ed454db3c69e72e
pad <pad@fb.com>
2015-05-19 10:50:38 -0700

    * yacc2/check.ml: refine

--- yacc2/check.ml
+++ yacc2/check.ml
@@ -1,3 +1,23 @@
+(* Yoann Padioleau
+ *
+ * Copyright (C) 2015 Yoann Padioleau
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation, with the
+ * special exception on linking described in file license.txt.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the file
+ * license.txt for more details.
+ *)
+open Ast
+
+(*****************************************************************************)
+(* Prelude *)
+(*****************************************************************************)
+
 (* TODO:
  * - exist 'start', and 'type' directives
  * - classic use/def: remember set of terms and non terms and look
@@ -5,3 +25,26 @@
  * - wrong $ number, too big
  * - typechecking (but this is done for free by ocaml in the generated code)
  *)
+
+(*****************************************************************************)
+(* Types *)
+(*****************************************************************************)
+
+type error = unit
+
+exception Error of error
+
+(*****************************************************************************)
+(* Helpers *)
+(*****************************************************************************)
+
+let report_error err =
+  failwith "TODO"
+
+(*****************************************************************************)
+(* Main entry point *)
+(*****************************************************************************)
+
+let check def =
+  failwith "TODO"
+
