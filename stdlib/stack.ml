4deee1f4231f2e3cf8c90585f9d696a105f92857
pad <pad@fb.com>
2015-05-19 15:35:34 -0700

    * stdlib/stack.ml: addons pad

--- stdlib/stack.ml
+++ stdlib/stack.ml
@@ -29,3 +29,15 @@ let pop s =
 let length s = List.length s.c
 
 let iter f s = List.iter f s.c
+
+(* addons pad *)
+let top_opt s =
+  match s.c with
+  | [] -> None
+  | x::xs -> Some x
+
+let top s =  
+  match s.c with
+  | x::xs -> x
+  | [] -> raise Empty
+
