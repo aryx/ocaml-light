7bfdac6efe172cb54f5aaddd3180a595f0355e18
pad <pad@fb.com>
2015-05-20 15:05:09 -0700

    * stdlib/obj.ml: remove obsolete func

--- stdlib/obj.ml
+++ stdlib/obj.ml
@@ -18,17 +18,13 @@ type t
 external repr : 'a -> t = "%identity"
 external obj : t -> 'a = "%identity"
 external magic : 'a -> 'b = "%identity"
+
 external is_block : t -> bool = "obj_is_block"
 external tag : t -> int = "obj_tag"
 external size : t -> int = "%obj_size"
 external field : t -> int -> t = "%obj_field"
 external set_field : t -> int -> t -> unit = "%obj_set_field"
 external new_block : int -> int -> t = "obj_block"
-
-let marshal (obj: t) =
-  Marshal.to_string obj []
-let unmarshal str pos =
-  (Marshal.from_string str pos, pos + Marshal.total_size str pos)
 
 (* todo:
 external is_int : t -> bool = "%obj_is_int"
