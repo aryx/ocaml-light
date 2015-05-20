bb106a53c6a9ee0b7a0fd4eb94ae73284066bf9e
pad <pad@fb.com>
2015-05-20 15:05:09 -0700

    * stdlib/obj.ml: remove obsolete func

--- stdlib/obj.mli
+++ stdlib/obj.mli
@@ -9,8 +9,6 @@
 (*                                                                     *)
 (***********************************************************************)
 
-(* $Id: obj.mli,v 1.13 1997/10/28 13:17:10 xleroy Exp $ *)
-
 (* Module [Obj]: operations on internal representations of values *)
 
 (* Not for the casual user. *)
@@ -20,18 +18,13 @@ type t
 external repr : 'a -> t = "%identity"
 external obj : t -> 'a = "%identity"
 external magic : 'a -> 'b = "%identity"
+
 external is_block : t -> bool = "obj_is_block"
 external tag : t -> int = "obj_tag"
 external size : t -> int = "%obj_size"
 external field : t -> int -> t = "%obj_field"
 external set_field : t -> int -> t -> unit = "%obj_set_field"
 external new_block : int -> int -> t = "obj_block"
-
-(* The following two functions are deprecated.  Use module [Marshal]
-   instead. *)
-
-val marshal : t -> string
-val unmarshal : string -> int -> t * int
 
 (* from 3.12 *)
 (*
