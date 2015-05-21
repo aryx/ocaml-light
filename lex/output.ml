bddf2dc68c1931228ee8415a4d97d59b2d137699
pad <pad@fb.com>
2015-05-21 10:59:26 -0700

    * lex/output.ml: simpler code generator

--- lex/output.ml
+++ lex/output.ml
@@ -119,4 +119,174 @@ let output_lexdef ic oc header tables entry_points trailer =
   (*e: [[Output.output_lexdef()]] generate entry points *)
   copy_chunk ic oc trailer
 (*e: function Output.output_lexdef *)
+
+(*****************************************************************************)
+(* Simpler version *)
+(*****************************************************************************)
+
+let debug = ref true
+
+(* 1- Generating the actions *)
+
+let output_action ic oc (i,act) =
+  output_string oc ("action_" ^ string_of_int i ^ " lexbuf = (\n");
+  if !debug 
+  then output_string oc (" log \"action_" ^ string_of_int i ^ "\";\n");
+  copy_chunk ic oc act;
+  output_string oc ")\nand ";
+  ()
+
+(* 2- Generating the states *)
+
+let states = ref ([||] : Lexgen.automata_matrix)
+
+let enumerate_vect v =
+  let rec enum env pos =
+    if pos >= Array.length v 
+    then env 
+    else
+      try
+        let pl = List.assoc v.(pos) env in
+          pl := pos :: !pl; enum env (succ pos)
+        with Not_found ->
+          enum ((v.(pos), ref [pos]) :: env) (succ pos) 
+  in
+  Sort.list
+    (fun (e1, pl1) (e2, pl2) -> List.length !pl1 >= List.length !pl2)
+    (enum [] 0)
+
+let output_move oc = function
+    Backtrack ->
+      if !debug
+      then output_string oc "log \"backtrack\"; ";
+      output_string oc "backtrack lexbuf"
+  | Goto dest ->
+      match !states.(dest) with
+        Perform act_num ->
+          output_string oc ("action_" ^ string_of_int act_num ^ " lexbuf")
+      | _ ->
+          (* Many states are just Perform so this explains why there is some
+           * big jumps in the generated files from e.g. state_3 to state_9
+           * without any intermediate state_4 function; it's because state 4
+           * was a Perform.
+           *)
+          output_string oc ("state_" ^ string_of_int dest ^ " lexbuf")
+
+let output_char_for_read oc = function
+    '\''  -> output_string oc "\\'"
+  | '\\' -> output_string oc "\\\\"
+  | '\n' -> output_string oc "\\n"
+  | '\t' -> output_string oc "\\t"
+  | c ->
+      let n = Char.code c in
+      if n >= 32 && n < 127 then
+        output_char oc c
+      else begin
+        output_char oc '\\';
+        output_char oc (Char.chr (48 + n / 100));
+        output_char oc (Char.chr (48 + (n / 10) mod 10));
+        output_char oc (Char.chr (48 + n mod 10))
+      end
+
+let rec output_chars oc = function
+    [] ->
+      failwith "output_chars"
+  | [c] ->
+      if c <= 255 then begin
+      output_string oc "'";
+      output_char_for_read oc (Char.chr c);
+      output_string oc "'"
+      end else output_string oc "'\\000'"
+  | c::cl ->
+      if c <= 255 then begin
+      output_string oc "'";
+      output_char_for_read oc (Char.chr c);
+      output_string oc "'|";
+      output_chars oc cl
+      end else output_string oc "'\\000'"
+
+let output_one_trans oc (dest, chars) =
+  output_chars oc !chars;
+  output_string oc " -> ";
+  output_move oc dest;
+  output_string oc "\n |  ";
+  ()
+
+let output_all_trans oc trans =
+  output_string oc "  let c = Lexing.get_next_char lexbuf in\n";
+  if !debug
+  then output_string oc "  log (\"consuming:\" ^ (Char.escaped c));\n";
+  output_string oc "  match c with\n    ";
+  match enumerate_vect trans with
+    [] ->
+      failwith "output_all_trans"
+  | (default, _) :: rest ->
+      List.iter (output_one_trans oc) rest;
+      output_string oc "_ -> ";
+      output_move oc default;
+      output_string oc "\nand ";
+      ()
+
+let output_state oc state_num = function
+    Perform i ->
+      ()
+  | Shift(what_to_do, moves) ->
+      output_string oc
+        ("state_"  ^ string_of_int state_num ^ " lexbuf =\n");
+      if !debug 
+      then output_string oc("  log \"state_" ^string_of_int state_num^ "\";\n");
+
+    (match what_to_do with
+      No_remember -> ()
+    | Remember i ->
+        output_string oc "  lexbuf.lex_last_pos <- lexbuf.lex_curr_pos;\n";
+        output_string oc ("  lexbuf.lex_last_action_simple <- Obj.magic action_" ^
+                             string_of_int i ^ ";\n")
+    );
+    output_all_trans oc moves
+
+(* 3- Generating the entry points *)
+          
+let rec output_entries oc = function
+    [] -> failwith "output_entries"
+  | entry :: rest ->
+      let name = entry.auto_name in
+      let state_num = entry.auto_initial_state in
+      output_string oc (name ^ " lexbuf =\n");
+      if !debug 
+      then output_string oc ("log \"" ^ name ^ "\";\n");
+      output_string oc "  Lexing.start_lexing lexbuf;\n";
+      output_string oc ("  state_" ^ string_of_int state_num ^ " lexbuf\n");
+      match rest with
+        [] -> output_string oc "\n"; ()
+      | _  -> output_string oc "\nand "; output_entries oc rest
+
+(* All together *)
+
+let output_lexdef_simple ic oc header (initial_st, st) trailer =
+  print_int (Array.length st); print_string " states, ";
+(*  print_int (List.length actions); print_string " actions."; *)
+  print_newline();
+  (* for the labels *)
+  output_string oc "open Lexing\n\n";
+  if !debug
+  then output_string oc
+    "let log x = print_endline (\"LEX: \" ^ x); flush stdout\n";
+  copy_chunk ic oc header;
+  output_string oc "\nlet rec ";
+  states := st;
+  initial_st |> List.iter (fun entry ->
+    let actions = entry.auto_actions in
+    List.iter (output_action ic oc) actions;
+    output_string oc "\n";
+  );
+  output_string oc "\n";
+  for i = 0 to Array.length st - 1 do
+    output_state oc i st.(i)
+  done;
+  output_string oc "\n";
+  output_entries oc initial_st;
+  output_string oc "\n";
+  copy_chunk ic oc trailer
+
 (*e: lex/output.ml *)
