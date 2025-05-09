cb2dc448b2f9b6bed12753060100997b62ba4cbd
pad <pad@r2c.dev>
2025-05-09 13:26:31 +0200

    add shell.nix and .github/workflows/nix.yml!

---
+++ shell.nix
@@ -0,0 +1,41 @@
+# Setup a dev environment for ocaml-light using Nix.
+# Run 'nix-shell --pure' from the root of the project to get a dev environment
+# ready to compile/test/run ocaml-light from Linux or macOS
+# (TODO: on amd64 or arm64).
+# See also .github/workflows/nix.yml for its use in Github Actions (GHA).
+# See also Dockerfile.
+# See also xix/shell.nix for more info about Nix.
+
+let
+   # fetch a specific nixos version for better reproducibility
+   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05";
+   pkgs = import nixpkgs { config = {}; overlays = []; };
+ in
+
+pkgs.mkShell {
+  packages = with pkgs; [
+     # Some Makefile relies on perl to fix some syncweb comments
+     # alt: use sed
+     perl
+     # Compile-time external libs, ex: pcre
+     # alt: disable otherlibs/graph/
+     xorg.libX11
+     # Optional utilities for development/debugging, ex: which
+     # Implicit utilities and libs installed by default in nix
+     # (see "echo $PATH | sed -e 's/:/\n/g'" and "ldd ./bin/hello-world"):
+     # - gnumake bash
+     # - binutils gcc/clang glibc linux-headers gnu-config update-autotools
+     # - coreutils findutils diffutils file gnugrep gnused
+     # - gnutar gzip bzip2 unzip xz brotli zlib zstd curl
+     # - not really needed but here: ed gawk patch patchelf
+   ];
+
+   #coupling: Dockerfile
+   shellHook = ''
+     #./configure
+     #make clean
+     #make coldstart
+     #make world
+     #make test
+  '';
+}
