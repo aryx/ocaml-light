411ea56bece3dc42c49211eb0f672d54403f4a56
pad <pad@r2c.dev>
2025-05-13 17:31:42 +0200

    errmsg.c: remove sys_errlist and sys_nerr ifdef case
    I already removed code checking for HAS_STRERROR before,
    not sure why, for configure to pass, but then there
    was still code guarded by it in otherlibs
    which caused some linking errors for xix/mk so
    I've removed the code there too

--- otherlibs/unix/errmsg.c
+++ otherlibs/unix/errmsg.c
@@ -17,8 +17,6 @@
 
 extern int error_table[];
 
-#ifdef HAS_STRERROR
-
 extern char * strerror(int);
 
 value unix_error_message(value err)
@@ -27,21 +25,3 @@ value unix_error_message(value err)
   errnum = error_table[Int_val(err)];
   return copy_string(strerror(errnum));
 }
-
-#else
-
-extern int sys_nerr;
-extern char *sys_errlist[];
-
-value unix_error_message(value err)
-{
-  int errnum;
-  errnum = error_table[Int_val(err)];
-  if (errnum < 0 || errnum >= sys_nerr) {
-    return copy_string("Unknown error");
-  } else {
-    return copy_string(sys_errlist[errnum]);
-  }
-}
-
-#endif
