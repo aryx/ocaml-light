2848361d6a3b19d90f2c1d8c82b23f8299f078dc
pad <yoann.padioleau@gmail.com>
2025-06-12 11:49:17 +0200

    stdcompat comment

--- stdlib/stdcompat.ml
+++ stdlib/stdcompat.ml
@@ -1,3 +1,8 @@
+(* When using ocaml-light, we can't also use dune and install the
+ * 'stdcompat' package so we provide a poor's man stdcompat
+ * that at least will allow to compile the xix code relying
+ * on stdcompat.
+ *)
 
 module Stdlib = struct
   let compare = Pervasives.compare
