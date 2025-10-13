b70bf4df9d214ca30a0a70a3e30d618652c2d6d8
pad <yoann.padioleau@gmail.com>
2025-10-13 11:38:53 +0200

    misc

--- stdlib/array.mli
+++ stdlib/array.mli
@@ -28,6 +28,7 @@ external set: 'a array -> int -> 'a -> unit = "%array_safe_set"
            0 to [Array.length a - 1].
            You can also write [a.(n) <- x] instead of [Array.set a n x]. *)
 external make: int -> 'a -> 'a array = "make_vect"
+(* DEPRECATED; use make *)
 external create: int -> 'a -> 'a array = "make_vect"
         (* [Array.make n x] returns a fresh array of length [n],
            initialized with [x].
