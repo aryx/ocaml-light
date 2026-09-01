6231bef5707d354aa9e35f17153d4f28bdfe9831
pad <yoann.padioleau@gmail.com>
2026-09-01 14:24:36 +0200

    restore alpha support from ocaml-github around 1.07

---
+++ asmcomp/alpha/reload.ml
@@ -0,0 +1,17 @@
+(***********************************************************************)
+(*                                                                     *)
+(*                           Objective Caml                            *)
+(*                                                                     *)
+(*            Xavier Leroy, projet Cristal, INRIA Rocquencourt         *)
+(*                                                                     *)
+(*  Copyright 1996 Institut National de Recherche en Informatique et   *)
+(*  Automatique.  Distributed only by permission.                      *)
+(*                                                                     *)
+(***********************************************************************)
+
+(* $Id$ *)
+
+(* Reloading for the Alpha *)
+
+let fundecl f =
+  (new Reloadgen.reload_generic)#fundecl f
